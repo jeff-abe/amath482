57456c8abe1227fc66afa31a4da58f3b0596d067
jeff-abe <62082006+jeff-abe@users.noreply.github.com>
2020-03-16 14:03:21 -0700

    Delete specwavelet.m

--- specwavelet.m
+++
@@ -1,9 +0,0 @@
-function data = specwavelet(signal,Fs)
-    clips = size(signal,2);
-    data = [];
-    for k = 1:clips 
-        s = signal(:,k)';
-        spec = cwt(s,Fs,'FrequencyLimits',[40 6000]);
-        data = [data abs(reshape(spec,[],1))];
-    end
-end
